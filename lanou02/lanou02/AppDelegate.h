41b108593a5a04b9811cd7b30590ad6e92901db4
lanou002 <1149779588@qq.com>
2015-08-14 10:04:08 +0800

    添加了工程

---
+++ lanou02/lanou02/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lanou02
+//
+//  Created by lanou on 15/8/14.
+//  Copyright (c) 2015年 abc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lanou02/lanou02/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lanou02
+//
+//  Created by lanou on 15/8/14.
+//  Copyright (c) 2015年 abc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
